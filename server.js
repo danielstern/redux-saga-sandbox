bc88a5c0089f238db548dfabd599800e5578fe77
Daniel Stern <daniel@danielstern.ca>
2017-07-17 13:22:57 -0400

    v1.0

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import cors from 'cors';
 import webpack from 'webpack';
 import webpackConfig from './webpack.config'
 import webpackDevMiddleware from 'webpack-dev-middleware';
-const compiler = webpack(webpackConfig);
 import webpackHotMiddleware from "webpack-hot-middleware";
-//
-let app = express();
-// console.log("Compiler?",compiler,webpackConfig)
+
+const compiler = webpack(webpackConfig);
+const port = process.env.PORT || 8080;
+const app = express();
+
 app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath,
@@ -18,12 +18,8 @@ app.use(webpackHotMiddleware(compiler, {
     'path': '/__webpack_hmr',
     'heartbeat': 10 * 1000
 }));
-//
-// app.use(cors());
-//
 app.use(express.static('public'));
-// app.use(express.static('public/css'));
-const port = process.env.PORT || 8080;
+
 app.listen(port,()=>{
     console.info(`Generator sandbox is listening on port ${port}.`);
 });
